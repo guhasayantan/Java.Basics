404717369b41fbdafbb62046bce36227c38350c8
Sayantan Guha <sayantanguha@hotmail.com>
2016-03-25 13:30:06 -0700

    Generic Class is added

---
+++ Java.Basics/src/Generic/GenericClass.java
@@ -0,0 +1,22 @@
+package Generic;
+
+public class GenericClass<T> {
+	
+	private T t;
+	public void set(T t)
+	{
+		this.t=t;
+	}
+	public T get()
+	{
+		return t;
+	}
+	
+	public static void main(String[] args) {
+		
+		GenericClass<Integer> intclass = new GenericClass<Integer>();
+		intclass.set(4);
+		System.out.println(intclass.get());
+	}
+
+}
