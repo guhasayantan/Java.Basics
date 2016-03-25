69d00809afc8985d39605c875cf7c469ba1c0761
Sayantan Guha <sayantanguha@hotmail.com>
2016-03-25 13:16:57 -0700

    Generic Method Demo added

---
+++ Java.Basics/src/Generic/GenericMethod.java
@@ -0,0 +1,29 @@
+package Generic;
+
+public class GenericMethod {
+	
+	public static <E> void printArray(E[] input)
+	{
+		for(E item: input)
+		{
+			System.out.printf("%s", item + " ");
+		}
+		System.out.println(" ");
+	}
+	
+	public static void main(String[] args) {
+		Integer[] intarr = {1, 2, 3, 4, 5};
+		Double[] doublearr = {1.0, 2.0, 3.0, 4.0};
+		Character[] chararr = {'H', 'E', 'L', 'L', 'O'};
+		
+		System.out.println("Integer Array: ");
+		printArray(intarr);
+		
+		System.out.println("Double Array: ");
+		printArray(doublearr);
+		
+		System.out.println("Character Array: ");
+		printArray(chararr);
+	}
+
+}
