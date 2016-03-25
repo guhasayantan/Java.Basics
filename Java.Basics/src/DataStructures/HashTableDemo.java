fa2a5f2aa8c1b5d4fe8e13118e67326dfc08b839
Sayantan Guha <sayantanguha@hotmail.com>
2016-03-25 12:03:56 -0700

    HashTable Demo added

---
+++ Java.Basics/src/DataStructures/HashTableDemo.java
@@ -0,0 +1,27 @@
+package DataStructures;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class HashTableDemo {
+
+	public static void main(String[] args) {
+		
+		Hashtable hashtable = new Hashtable();
+		hashtable.put("Sayantan", 1);
+		hashtable.put("Shaan", 2);
+		hashtable.put("Priyanka", 3);
+		hashtable.put("Dolly", 4.0);
+		
+		
+		System.out.println("Printing hashtable: " + hashtable);
+		
+		Enumeration enumeration = hashtable.keys();
+		System.out.println("Iterating hashtable: ");
+		while(enumeration.hasMoreElements())
+		{
+			System.out.println(enumeration.nextElement());
+		}
+	}
+
+}
