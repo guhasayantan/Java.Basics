5ebb113900100f0ad55a10e2ee19bda6246211a3
Sayantan Guha <sayantanguha@hotmail.com>
2016-04-03 21:24:10 -0700

    InputOutput is added

---
+++ Java.Basics/src/InputOutput/ConcatinationDemo.java
@@ -0,0 +1,11 @@
+package InputOutput;
+
+public class ConcatinationDemo {
+
+	public static void main(String[] args) {
+		System.out.println(2+0+1+6+"Sayantan"+2+0+1+6);
+		System.out.println(2+0+1+6+"Sayantan"+(2+0+1+6));
+
+	}
+
+}
